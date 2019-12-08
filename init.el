ea177679ccc813f73dbfa90291cf8808f052a060
ansxor_ <answerxox@gmail.com>
2019-12-07 20:29:49 -0500

    owo

--- init.el
+++ init.el
@@ -6,12 +6,10 @@
  '(custom-safe-themes
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "36ca8f60565af20ef4f30783aa16a26d96c02df7b4e54e9900a5138fb33808da" default)))
- '(epg-gpg-program "C:/Program Files (x86)/gnupg/bin/gpg.exe")
- '(org-agenda-files (quote ("~/.emacs.d/School.org.gpg")))
  '(org-startup-indented t)
  '(package-selected-packages
    (quote
-    (fireplace emacs-fireplace autocomplete-mode company company-mode ox-reveal org-reveal google-translate ido-vertical-mode projectile auto-complete autocomplete smart-mode-line w3m powerthesaurus powerline popup-kill-ring beacon ample-theme all-the-icons neotree which-key auto-package-update htmlize dracula-theme use-package))))
+    (rust-mode clojure-mode xah-fly-keys fireplace emacs-fireplace autocomplete-mode company company-mode ox-reveal org-reveal google-translate ido-vertical-mode projectile auto-complete autocomplete smart-mode-line w3m powerthesaurus powerline popup-kill-ring beacon ample-theme all-the-icons neotree which-key auto-package-update htmlize dracula-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
